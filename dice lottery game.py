4266427364a3c7c0fa2ce7fe88d7bcc25d890082
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-20 22:02:12 +0530

    module file

---
+++ dice lottery game.py
@@ -0,0 +1,27 @@
+import useful_tools
+
+num = int(input("Enter the dice size : "))
+counter = 0
+end_counter = 2
+numbers = [3,6,9,11]
+
+while counter < end_counter:   
+    counter += 1
+
+
+    dice_result = useful_tools.roll_dice(num)
+    if  dice_result in numbers:
+        print(f"The dice rolled: {dice_result}")
+        print("You won the lottery")
+        break
+    else:
+        
+        print(f"The dice rolled: {dice_result}")
+        if counter <end_counter:
+            print(f"You have {end_counter - counter} chances left")
+            print(f"Sorry try again")
+            num = int(input("Enter the dice size : "))
+        else:
+            print("You lost, No more chances left. Game over.")
+
+
