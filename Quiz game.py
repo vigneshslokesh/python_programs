1c6469b1e2815c02da7ded4701511d2486af4c2d
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-21 22:55:51 +0530

    Quiz game

--- Quiz game.py
+++ Quiz game.py
@@ -1,9 +1,9 @@
 from Question import Question 
 
 question_prompt = [
-    "What colors are apple?\n(a) Red/Green\n (b) Purple\n(c) Orange\n\n",
-    "What colors are bananas?\n(a) Teal\n (b) Magenta\n(c) Yellow\n\n",
-    "What colors are strawberries?\n(a) Yellow\n (b) Red\n(c) Blue\n\n",
+    "What colors are apple?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What colors are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What colors are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n",
 ]
 
 questions = [
@@ -11,3 +11,13 @@ questions = [
     Question(question_prompt[1], "c"),
     Question(question_prompt[2], "b"),
 ]
+
+def runtest(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.ans:
+            score += 1
+    print("You got "+ str(score) + "/" + str(len(questions)) + "Correct")
+
+runtest(questions)   
