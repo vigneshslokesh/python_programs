a62112e58ef6cc712852d84133fd794ec6b42799
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-06-22 23:08:37 +0530

    helper class example

---
+++ helper class.py
@@ -0,0 +1,16 @@
+class StringHelper:
+    def count_vowels(s): #function to count the number of vowels for a given input 
+        vowels = "aAeEiIoOuU"
+        return sum(1 for char in s if char in vowels)
+    
+    def reverse_string(s): #function to return the reverse of a input string
+        return s[::-1]
+
+helper = StringHelper #assigning the helper class to a variable called helper
+
+ip = input("Enter the String : ")
+choice = input("Select your choice : \n(a) count vowels --> a\n(b) reverse string --> b\n")
+if choice == "a" :
+    print(f"The number of vowles are : {helper.count_vowels(ip)}")
+
+
